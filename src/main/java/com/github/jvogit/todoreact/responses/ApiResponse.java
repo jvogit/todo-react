a996b3ad327b3af2af7c62d9a4da923ddcdc5f0b
jvogit <justinxvopro@gmail.com>
2020-12-30 22:42:28 -0800

    clean up response handling a bit

--- src/main/java/com/github/jvogit/todoreact/responses/ApiResponse.java
+++ src/main/java/com/github/jvogit/todoreact/responses/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.github.jvogit.todoreact.responses;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import lombok.Data;
 
@@ -8,4 +9,9 @@ import lombok.Data;
 public class ApiResponse {
     private final HttpStatus status;
     private final String message;
+    
+    public ResponseEntity<Object> toResponseEntity() {
+        return ResponseEntity.status(status).body(this);
+    }
+    
 }
