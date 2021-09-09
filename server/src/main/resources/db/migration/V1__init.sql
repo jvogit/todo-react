63eda073b0684c0b92d8280fb8cb28416e9fb8e9
jvogit <justinxvopro@gmail.com>
2021-09-09 01:08:57 -0700

    genrate initial flyway migration

--- server/src/main/resources/db/migration/V1__init.sql
+++ server/src/main/resources/db/migration/V1__init.sql
@@ -1,10 +1,22 @@
 CREATE SEQUENCE IF NOT EXISTS hibernate_sequence START WITH 1 INCREMENT BY 1;
 
+CREATE TABLE todos
+(
+    id         UUID NOT NULL,
+    created_at TIMESTAMP with time zone,
+    updated_at TIMESTAMP with time zone,
+    completed  BOOLEAN,
+    item       VARCHAR(255),
+    pos        INTEGER,
+    user_id    UUID,
+    CONSTRAINT pk_todos PRIMARY KEY (id)
+);
+
 CREATE TABLE users
 (
     id            UUID NOT NULL,
-    created_at    TIMESTAMP WITHOUT TIME ZONE,
-    updated_at    TIMESTAMP WITHOUT TIME ZONE,
+    created_at    TIMESTAMP with time zone,
+    updated_at    TIMESTAMP with time zone,
     username      VARCHAR(255),
     email         VARCHAR(255),
     password      VARCHAR(255),
@@ -17,3 +29,6 @@ ALTER TABLE users
 
 ALTER TABLE users
     ADD CONSTRAINT uc_users_username UNIQUE (username);
+
+ALTER TABLE todos
+    ADD CONSTRAINT FK_TODOS_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
